3607ecd28bf25fe181b5ca030c991a33e8759558
Resika-MD <vmaster2215@gmail.com>
2024-12-18 09:02:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_IDcusyzboZ#MR1FBJnkl2FwAHJUFoPSmczQveUiV9dkuGn9dw2Qkrg,
+SESSION_ID: process.env.SESSION_ID || "cusyzboZ#MR1FBJnkl2FwAHJUFoPSmczQveUiV9dkuGn9dw2Qkrg",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/106251140?v=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "This Bot Create By Resika Hirushan I'm Alive Now",
 };
